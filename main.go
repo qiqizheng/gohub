0fcf93a0d80f5224c046a341ce50d515357ab413
qiqizheng <417542931@qq.com>
2023-03-06 10:22:38 +0800

    集成 gin

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -22,6 +23,23 @@ func main() {
 		})
 	})
 
+	//处理404请求
+	r.NoRoute(func(c *gin.Context) {
+		//获取标头信息的 Accept 信息
+		acceptString := c.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			//如果是HTML的话
+			c.String(http.StatusNotFound, "页面返回404")
+		} else {
+			//默认返回json
+			c.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "路由未定义， 请确认 url 和请求方法是否正确",
+			})
+		}
+
+	})
+
 	//运行服务
-	r.Run()
+	r.Run(":8009")
 }
